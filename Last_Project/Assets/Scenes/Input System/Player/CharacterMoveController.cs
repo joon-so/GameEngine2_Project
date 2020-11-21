2b1955e18ea28a8871aaa4d50c336afa5e43de37
So Joon <joonso16@gmail.com>
2020-11-21 11:08:43 +0900

    player controll edit

---
+++ Last_Project/Assets/Scenes/Input System/Player/CharacterMoveController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NewBehaviourScript : MonoBehaviour, GameInputAction.IFpsActions
+{
+    private CharacterController _characterController;
+    void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+    }
+
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+    }
+}
