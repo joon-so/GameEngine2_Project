8503e1377daa41c4c2db6b522880e372f453dfb5
Lee Mi Reuk <sslejds@naver.com>
2020-11-27 03:14:58 +0900

    Add Player and Enemy

---
+++ GameEngine2/Assets/Scripts/EnemyFSM.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFSM : MonoBehaviour
+{
+    // Enemy 상태 상수
+    enum EnemyState
+    {
+        Idle,
+        Move,
+        Attack,
+        Return,
+        Damaged,
+        Die
+    }
+
+    // Enemy 상태 변수
+    EnemyState enemyState;
+
+    GameObject player;
+    
+    CharacterController cc;
+
+    public float findDistance = 8f;
+    public float attackDistance = 2f;
+    public float moveSpeed = 5f;
+    public float attackDelayTime = 2f;
+    float currentTime = 0;
+    public int attackPower = 2;
+    public int maxHp = 5;
+    int currentHp;
+    // 초기 위치 저장
+    Vector3 originPos;
+    // 이동 가능한 거리
+    public float moveDistance = 20f;
+
+
+    void Start()
+    {
+        enemyState = EnemyState.Idle;
+        player = GameObject.Find("Player");
+        cc = GetComponent<CharacterController>();
+        originPos = transform.position;
+        currentHp = maxHp;
+    }
+
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Move:
+                Move();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Return:
+                Return();
+                break;
+            case EnemyState.Damaged:
+                Damaged();
+                break;
+            case EnemyState.Die:
+                Die();
+                break;
+        }
+    }
+
+    void Idle()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) <= findDistance)
+        {
+            enemyState = EnemyState.Move;
+            print("상태 전환: Idle->Move");
+        }
+    }
+
+    void Move()
+    {
+        if (Vector3.Distance(originPos, transform.position) > moveDistance)
+        {
+            enemyState = EnemyState.Return;
+            print("상태 전환: Move->Return");
+        }
+
+        else if (Vector3.Distance(player.transform.position, transform.position) > attackDistance)
+        {
+            Vector3 dir = (player.transform.position - transform.position).normalized;
+
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            enemyState = EnemyState.Attack;
+            print("상태 전환: Move->Attack");
+
+            currentTime = attackDelayTime;
+        }
+    }
+
+    void Attack()
+    {
+        if (Vector3.Distance(player.transform.position, transform.position) <= attackDistance)
+        {
+    
+            if (currentTime >= attackDelayTime)
+            {
+                currentTime = 0;
+                print("공격");
+                PlayerMove pm = player.GetComponent<PlayerMove>();
+                pm.OnDamage(attackPower);
+            }
+            else
+                currentTime += Time.deltaTime;
+
+        }
+        else
+        {
+            enemyState = EnemyState.Move;
+            print("상태 전환: Attack->Move");
+        }
+    }
+
+    void Return()
+    {
+        if (Vector3.Distance(originPos, transform.position) > 0.1)
+        {
+            Vector3 dir = (originPos - transform.position).normalized;
+            cc.Move(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = originPos;
+            enemyState = EnemyState.Idle;
+            print("상태 전환: Return->Idle");
+
+            currentHp = maxHp;
+        }
+    }
+
+    void Damaged()
+    {
+
+    }
+
+    void Die()
+    {
+
+    }
+
+    public void HitEnemy(int value)
+    {
+        currentHp -= value;
+
+        if (currentHp > 0)
+        {
+            enemyState = EnemyState.Damaged;
+            print("상태 전환: state->Damaged");
+        }
+        else
+        {
+            enemyState = EnemyState.Die;
+            print("상태 전환: ->die");
+        }
+    }
+
+}
---
+++ GameEngine2/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    CharacterController cc;
+
+    public float moveSpeed = 7f;
+    public float jumpPower = 1f;
+    public float yVelocity = 0;
+    float gravity = -20f;
+    public int maxJump = 1;
+    int jumpCount = 0;
+
+
+    public int hp = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();
+
+        if (cc.collisionFlags == CollisionFlags.Below)
+        {
+            jumpCount = 0;
+            yVelocity = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJump)
+        {
+            jumpCount++;
+            yVelocity = jumpPower;
+        }
+
+        yVelocity += gravity * Time.deltaTime;
+        dir.y = yVelocity;
+
+        cc.Move(dir * moveSpeed * Time.deltaTime);
+    }
+
+    public void OnDamage(int value)
+    {
+        hp -= value;
+        if (hp<0)
+        {
+            hp = 0;
+        }
+    }
+}
