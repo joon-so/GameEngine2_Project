e0dd855f5f4d097e8b21317152bfddaa4dd4b9c7
So Joon <joonso16@gmail.com>
2020-12-03 17:07:01 +0900

    Camera Move

--- GameEngine2/Assets/Scripts/Follow.cs
+++ GameEngine2/Assets/Scripts/Follow.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Follow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
+    public GameObject target;
+    public float smoothig = 1.0f;
+    public float dist = 8f;
+    public float height = 20f;
 
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position = target.position + offset;
+        float currYAngle = Mathf.LerpAngle(transform.eulerAngles.y, target.transform.eulerAngles.y,
+            smoothig * Time.deltaTime);
+
+        Quaternion rot = Quaternion.Euler(0, currYAngle, 0);
+
+        transform.position = target.transform.position - (rot * Vector3.forward * dist)
+            + (Vector3.up * height);
+
+        transform.LookAt(target.transform);
     }
 }
