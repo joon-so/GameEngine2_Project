571bafd03625d290a471d0c697cbabc163394067
Lee Mi Reuk <sslejds@naver.com>
2020-12-07 05:25:14 +0900

    Cinemachine

---
+++ GameEngine2/Assets/Scripts/Plane.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plane : MonoBehaviour
+{
+    bool changeTarget;
+    Vector3 target1 = new Vector3(-80, 50, -100);
+    Vector3 target2 = new Vector3(0, 10, 0);
+
+    private void Start()
+    {
+        changeTarget = false;
+    }
+
+
+    void Update()
+    {
+        if (!changeTarget)
+            transform.position = Vector3.MoveTowards(transform.position, target1, 1f);
+        
+        if (transform.position == target1)
+        {
+            changeTarget = true;
+        }
+
+        if (changeTarget)
+            transform.position = Vector3.Slerp(transform.position, target2, 0.005f);
+
+
+
+
+    }
+}
