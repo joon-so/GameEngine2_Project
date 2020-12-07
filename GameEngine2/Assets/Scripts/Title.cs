d0efee82660c1d38c9872d44f8cc6366ac0810e8
So Joon <joonso16@gmail.com>
2020-12-07 21:54:44 +0900

    make title

---
+++ GameEngine2/Assets/Scripts/Title.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public GameObject manual;
+    bool manual_look = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            manual_look = false;
+            manual.gameObject.SetActive(manual_look);
+        }
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadSceneAsync("SampleScene");
+    }
+
+    public void ManualButton()
+    {
+        if (!manual_look)
+        {
+            manual_look = true;
+            manual.gameObject.SetActive(manual_look);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
